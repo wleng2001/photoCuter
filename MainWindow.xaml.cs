c84b098f81577f3d07447424abf564cb4b837e42
wleng2001 <kamilgrodzki5@gmail.com>
2024-05-20 19:22:52 +0200

    add rectangle to cut photo (not resizable yet)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,8 @@ namespace photoCuter
     public partial class MainWindow : Window
     {
         string[] files;
+        Rectangle cutRectangle;
+        BitmapImage bI;
         public MainWindow()
         {
             InitializeComponent();
@@ -36,28 +38,37 @@ namespace photoCuter
             settingConstrastTextBox.Text = Math.Round(contrastSlider.Value, 1).ToString();
         }
 
+        void setSliderWithTextBox(float min, float max, TextBox textBox, Slider slider)
+        {
+            float value;
+            if (float.TryParse(textBox.Text, out value))
+                if (value >= min && value <= max)
+                    slider.Value = value;
+                else
+                    MessageBox.Show("Out of the range");
+            else
+                MessageBox.Show("Wrong format");
+        }
         private void settingConstrastTextBox_ClickEnter(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Enter)
             {
-                float value;
-                if (float.TryParse(settingConstrastTextBox.Text, out value))
-                    if (value >= -10 && value <= 10)
-                        contrastSlider.Value = value;
-                    else
-                        MessageBox.Show("Out of the range");
-                else
-                    MessageBox.Show("Wrong format");
+                setSliderWithTextBox(-10, 10, settingConstrastTextBox, contrastSlider);
             }
 
         }
 
+        private void settinBrightnessTextBox_ClickEnter(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                setSliderWithTextBox(-10, 10, settingBrightnessTextBox, brightnessSlider);
+        }
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        private async void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             var dialogForm = new Microsoft.Win32.OpenFileDialog();
             dialogForm.Filter = "Image files (*.bmp, *.jpg, *.png)|*.bmp;*.jpg; *.png|All files (*.*)|*.*";
@@ -68,10 +79,54 @@ namespace photoCuter
             if(result == true)
             {
                 files = dialogForm.FileNames;
-                BitmapImage bI = new BitmapImage(new Uri(files[0]));
+                bI = new BitmapImage(new Uri(files[0]));
                 ImageBrush iB = new ImageBrush(bI);
                 iB.Stretch = Stretch.Uniform;
                 photoCanvas.Background = iB;
+                putCutRectangle(photoCanvas.ActualWidth, photoCanvas.ActualHeight);
+
+            }
+        }
+
+        void putCutRectangle(double windowWidth, double windowHeight)
+        {
+            double width;
+            double height;
+            double x;
+            double y;
+            photoCanvas.Children.Remove(cutRectangle);
+            if (bI.Width > bI.Height && windowWidth/windowHeight < bI.Width/bI.Height)
+            {
+                width = windowWidth;
+                x = 0;
+                height = (bI.Height*windowWidth)/bI.Width;
+                y = (windowHeight-height)/2;
+            }
+            else
+            {
+                height = windowHeight;
+                y = 0;
+                width = (bI.Width*windowHeight)/bI.Height;
+                x =  (windowWidth - width) /2 ;
+            }
+            cutRectangle = new Rectangle();
+            cutRectangle.Width = width;
+            cutRectangle.Height = height;
+            cutRectangle.Stroke = Brushes.Black;
+            photoCanvas.Children.Add(cutRectangle);
+            Canvas.SetLeft(cutRectangle, x);
+            Canvas.SetTop(cutRectangle, y);
+        }
+
+        private void photoCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Main_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(bI != null)
+            {
+                putCutRectangle(photoCanvas.ActualWidth, photoCanvas.ActualHeight);
             }
         }
     }
